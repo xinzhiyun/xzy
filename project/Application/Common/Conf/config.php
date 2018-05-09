5bf14a65303555412da6bf842cec0944012fd6ed
潘宏钢 <shipinluzhi@panhonggangdeMacBook-Pro.local>
2018-05-09 17:03:25 +0800

    修改数据库配置

--- project/Application/Common/Conf/config.php
+++ project/Application/Common/Conf/config.php
@@ -2,12 +2,12 @@
 return array(
 	/* 数据库设置 */
     'DB_TYPE'                => 'mysql', // 数据库类型
-    // 'DB_HOST'                => '120.27.12.1', // 服务器地址
-   'DB_HOST'                => '127.0.0.1', // 服务器地址
+    'DB_HOST'                => '120.27.12.1', // 服务器地址
+   // 'DB_HOST'                => '127.0.0.1', // 服务器地址
     'DB_NAME'                => 'bluetooth', // 数据库名
 //    'DB_NAME'                => 'pubdata', // 数据库名
     'DB_USER'                => 'root', // 用户名
-    'DB_PWD'                 => '123', // 密码
+    'DB_PWD'                 => 'root', // 密码
     'DB_PORT'                => '3306', // 端口
     'DB_PREFIX'              => 'pub_', // 数据库表前缀
     'DB_PARAMS'              => array(), // 数据库连接参数
