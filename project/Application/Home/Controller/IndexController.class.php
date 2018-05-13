b646fb461eaa83104c1c1c2d77ae1cce4ecc08c9
梁鼠标 <liangshubiao@liangshubiaodeMacBook-Pro.local>
2018-05-13 17:10:00 +0800

    梁康伦的修改—获取openid

--- project/Application/Home/Controller/IndexController.class.php
+++ project/Application/Home/Controller/IndexController.class.php
@@ -12,10 +12,12 @@ class IndexController extends CommonController
     public function index()
     {
         //在充值处获取客户id
-        $auid = $_GET['auid'];
-
+        if (empty($_SESSION['auid'])) {
+            $auid = $_SESSION['auid'] = $_GET['auid'];
+        }
+        
         // 根据客户id获取客户微信公众号信息
-        $info = M('adminuser')->where('id='.$auid)->find();
+        $info = M('adminuser')->where('id='.$_SESSION['auid'])->find();
 
         $appid = $info['appid'];
         $appsecret = $info['appsecret'];
@@ -23,6 +25,13 @@ class IndexController extends CommonController
         $weixin  = new WeixinJssdk($appid, $appsecret);
 
         $openId_ifno = $weixin->getSignPackage();
+        
+
+        $openid = $weixin->GetOpenid();
+
+        if (empty($_SESSION['openid'])) {
+            $_SESSION['openid'] = $openid;
+        }
 
     	$this->assign('weixin',$openId_ifno);
         $this->display();
