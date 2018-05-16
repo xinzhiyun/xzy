ae432952aa5bdc9f8a878b9128c2298e113d2b12
梁鼠标 <liangshubiao@liangshubiaodeMacBook-Pro.local>
2018-05-16 10:20:28 +0800

    梁康伦的修改—查询设备剩余天数

--- project/Application/Home/Controller/IndexController.class.php
+++ project/Application/Home/Controller/IndexController.class.php
@@ -205,9 +205,34 @@ class IndexController extends Controller
      * [getNum 点击查询的时候获取设备剩余天数]
      * @return [type] [description]
      */
-    public function getNum()
-    {
+    public function select()
+    {   
+        //在查询处获取客户id
+        if (empty($_SESSION['auid'])) {
+            $auid = $_SESSION['auid'] = $_GET['auid'];
+        }
+        
+        // 根据客户id获取客户微信公众号信息
+        $info = M('adminuser')->where('id='.$_SESSION['auid'])->find();
 
+        $appid = $info['appid'];
+        $appsecret = $info['appsecret'];
+
+        $weixin  = new WeixinJssdk($appid, $appsecret);
+
+        $openId_ifno = $weixin->getSignPackage();
+        
+        // dump($openId_ifno);
+        $openid = $weixin->GetOpenid();
+
+        if (empty($_SESSION['openid'])) {
+            $_SESSION['openid'] = $openid;
+        }
+
+        $this->assign('weixin',$openId_ifno);
+        $this->assign('openid',$_SESSION['openid']);
+        $this->display();
+        $this->display();
     }
 
 
