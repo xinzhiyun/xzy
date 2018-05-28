f5ae6e59ff7696add7f126367cacd4badc520e0e
潘宏钢 <shipinluzhi@panhonggangdeMacBook-Pro.local>
2018-05-28 18:57:09 +0800

    去掉产品型号限制

--- project/Application/Admin/Model/ProductModel.class.php
+++ project/Application/Admin/Model/ProductModel.class.php
@@ -14,9 +14,9 @@ class ProductModel extends Model
     // 自动验证
     protected $_validate = array(
        array('typename','require','型号名称不能为空'),
-       array('product_id','require','微信product_id不能为空'),
+       array('product_id','require','微信product_id不能为空')
 //        array('typename','/^[a-zA-Z0-9\x{4e00}-\x{9fa5}]{1,660}$/u','类型名称不能使用特殊字符',1,'regex'),
-        array('typename','','该型号名称已存在，请换一个试试，如净水器A型',0,'unique',1)
+        // array('typename','','该型号名称已存在，请换一个试试，如净水器A型',0,'unique',1)
         
         // array('url','/@(?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'".,<>?«»“”‘’]))@','网址格式不对',1,'regex')
     );
